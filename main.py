8b956aa175c9d80d55a15af8a3e424e5d5e5a4da
leon <leonlu.cc@foxmail.com>
2025-02-17 11:17:49 +0800

    更新pyinstaller安装配置

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ logger = mylogger.get_logger(__name__)
 
 if __name__ == "__main__":
     logger.info("Starting pptflow...")
-    from pptflow.gui.ppt2video_flow import PPTFlowApp
     start_time = time.time()  # record start time
+    from pptflow.gui.ppt2video_flow import PPTFlowApp
     app = PPTFlowApp()
     end_time = time.time()  # record end time
     logger.info(f"Application started. Startup time: {end_time - start_time:.2f} seconds.")
